f969de38a1fefb8bd408c92538101a903cfe9301
Eladamar <1eladamar1@gmail.com>
2021-03-10 14:39:40 +0200

    support in multystyle

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-import torch
+import os
+
+# import torch
 import torch.optim as optim
 from PIL import Image
 import torchvision.transforms as transforms
-from torchvision.utils import save_image
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from datetime import datetime
 
-from model import ImageTransformUNet
-from VGG import MyVgg
-
-
-def load_image(image_name):
-    image = Image.open(image_name)
-    image = loader(image).unsqueeze(0)
-    return image.to(device)
+from Unet import UNet
+from VGG import MyVgg16, MyVgg19
+from utils import *
+from train import Trainer
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-imsize = 512
 
+# Hyperparameters
+learning_rate = 0.001
+batch_size = 8
+imsize = 256
 
-loader = transforms.Compose(
+# Root directory of the dataset
+data_root = '/content/data'
+style_folder = '/content/drive/MyDrive/StyleTransfer/styles'
+if not os.path.isdir(data_root):
+  raise Exception(f'No such folder {data_root}')
+
+transformer = transforms.Compose(
     [
         transforms.Resize((imsize, imsize)),
         transforms.ToTensor(),
@@ -26,63 +35,34 @@ loader = transforms.Compose(
     ]
 )
 
-y_c = load_image("annahathaway.png")
-y_s = load_image("style.jpg")
+# load data
 
-VGG = MyVgg().to(device).eval()
-loss_mse = torch.nn.MSELoss()
+styles = ImageFolder(style_folder, transformer)
+print(styles.imgs)
+y_s = torch.stack([i for i,_ in ImageFolder(style_folder, transformer)])
 
-image_transformer = ImageTransformUNet().to(device)
-image_transformer.train()
 
-# Hyperparameters
-total_steps = 100
-learning_rate = 0.001
-alpha = 1
-beta = 0.01
+
+dataset = ImageFolder(data_root, transformer)
+# dataset = torch.utils.data.Subset(dataset, range(30000))
+data_loader = DataLoader(dataset=dataset, batch_size=batch_size, num_workers=8, drop_last=True)
+
+# models
+VGG = MyVgg16()
+image_transformer = UNetSmall(batch_size)
+
 optimizer = optim.Adam(image_transformer.parameters(), lr=learning_rate)
+criterion = torch.nn.MSELoss()
+log_name = f'{type(image_transformer).__name__}_{datetime.now().strftime("%m%d-%H")}'
 
-for step in range(total_steps):
+trainer = TrainerMul(image_transformer=image_transformer,
+                vgg=VGG,
+                optimizer=optimizer,
+                criterion=criterion,
+                style=y_s,
+                device=device,
+                tensorboard_logger=log_name)
 
-    y_c_features = VGG(y_c)
-    y_s_features = VGG(y_s)
+trainer.train(data_loader, 2)
 
-    y_hat = image_transformer(y_c)
-    y_hat_features = VGG(y_hat)
 
-    # Loss is 0 initially
-    style_loss = original_loss = 0
-
-    # iterate through all the features for the chosen layers
-    for gen_feature, orig_feature, style_feature in zip(
-            y_hat_features, y_c_features, y_s_features
-    ):
-
-        # batch_size will just be 1
-        batch_size, channel, height, width = gen_feature.shape
-
-        #original_loss += torch.mean((gen_feature - orig_feature) ** 2)
-        original_loss += loss_mse(gen_feature, orig_feature)
-
-        # Compute Gram Matrix of generated
-        G = gen_feature.view(channel, height * width).mm(
-            gen_feature.view(channel, height * width).t()
-        )
-        # Compute Gram Matrix of Style
-        A = style_feature.view(channel, height * width).mm(
-            style_feature.view(channel, height * width).t()
-        )
-        #style_loss += torch.mean((G - A) ** 2)
-        style_loss += loss_mse(G, A)
-
-    total_loss = alpha * original_loss + beta * style_loss
-    optimizer.zero_grad()
-    total_loss.backward()
-    optimizer.step()
-
-    if step % 5 == 0:
-        print(total_loss)
-        image_transformer.eval()
-        generated = image_transformer(y_c)
-        save_image(generated, "generated.png")
-        image_transformer.train()

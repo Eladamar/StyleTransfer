4f7f35524f1e85331ed3954961b410a6c607beff
Eladamar <1eladamar1@gmail.com>
2021-03-21 11:06:54 +0200

    added arg parser

--- inference.py
+++ inference.py
@@ -1,31 +1,59 @@
+import argparse
+
 import torch
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from PIL import Image
 
-from Unet import UNet, UNetSmall
-from ITN import ImageTransformNet
-
+from Unet import UNet
 from utils import load_image
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-transformer = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(256),
-        transforms.ToTensor()
-])
+def style_transfer(style_model, y_c, style, output):
+    stylized = style_model(y_c, torch.tensor([style]).to(device))
+    stylized = transforms.Resize(ARGS.output_size)(stylized).cpu()
+    save_image(stylized.data[0], f'{ARGS.output}/gen{style}.jpg')
 
-def style_transfer():
+def main(ARGS):
+
+    transformer = transforms.Compose([
+        transforms.Resize(ARGS.input_size),
+        transforms.CenterCrop(ARGS.input_size),
+        transforms.ToTensor()
+    ])
   
-    y_c = load_image("annahathaway.png", transformer).to(device)
+    y_c = load_image(ARGS.content_image, transformer).to(device)
 
     # load style model
-    style_model = UNetSmall(8).to(device)
-    style_model.load_state_dict(torch.load('UNetSmall_0309-15.model'))
+    style_model = UNet(ARGS.styles_num).to(device)
+    style_model.load_state_dict(torch.load('UNet_0316-15.model'))
 
     # process input image
-    stylized = style_model(y_c, torch.tensor([1]).to(device)).cpu()
-    save_image(stylized.data[0], 'gen.jpg')
+    if ARGS.style != -1:
+        style_transfer(style_model, ARGS.content_image, ARGS.style, ARGS.output)
+        return
+
+    for style in range(ARGS.styles_num):
+        style_transfer(style_model, ARGS.content_image, style, ARGS.output)
+
 
 if __name__ == "__main__":
-  style_transfer()
+    parser = argparse.ArgumentParser(description='Style An Image')
+    parser.add_argument('--model', type=str, default='./model.model',
+                        help='Path to model')
+    parser.add_argument('--content_image', type=str, default='./content.jpg',
+                        help='Path to content image')
+    parser.add_argument('-sn', '--styles_num', type=int, default=16,
+                        help='Number of Style images trained in model')
+    parser.add_argument('--input_size', type=int, default=256,
+                        help='Image size used in training')
+    parser.add_argument('--output_size', type=int, default=256,
+                        help='Stylized output size')
+    parser.add_argument('--style', nargs='+', type=int,
+                        help='Style indices to use on image')
+    parser.add_argument('--output', type=str, default='./',
+                        help='Path to output folder')
+    ARGS = parser.parse_args()
+
+    main(ARGS)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
+import argparse
 
-# import torch
 import torch.optim as optim
 from PIL import Image
 import torchvision.transforms as transforms
@@ -13,56 +13,65 @@ from VGG import MyVgg16, MyVgg19
 from utils import *
 from train import Trainer
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Hyperparameters
-learning_rate = 0.001
-batch_size = 8
-imsize = 256
+def main(ARGS):
 
-# Root directory of the dataset
-data_root = '/content/data'
-style_folder = '/content/drive/MyDrive/StyleTransfer/styles'
-if not os.path.isdir(data_root):
-  raise Exception(f'No such folder {data_root}')
+    data_root = ARGS.data_root
+    if not os.path.isdir(data_root):
+        raise Exception(f'No such folder {data_root}')
 
-transformer = transforms.Compose(
-    [
-        transforms.Resize((imsize, imsize)),
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ]
-)
+    transformer = transforms.Compose(
+        [
+            transforms.Resize((ARGS.image_size, ARGS.image_size)),
+            transforms.ToTensor(),
+            # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
 
-# load data
+    # load data
+    styles = ImageFolder(ARGS.style_folder, transformer)
+    print(styles.imgs)
+    y_s = torch.stack([i for i, _ in styles])
 
-styles = ImageFolder(style_folder, transformer)
-print(styles.imgs)
-y_s = torch.stack([i for i,_ in ImageFolder(style_folder, transformer)])
+    dataset = ImageFolder(data_root, transformer)
+    # dataset = torch.utils.data.Subset(dataset, range(30000))
+    data_loader = DataLoader(dataset=dataset, batch_size=ARGS.batch_size, num_workers=8, drop_last=True)
 
+    # models
+    VGG = MyVgg16()
+    image_transformer = UNet(len(styles))
 
+    optimizer = optim.Adam(image_transformer.parameters(), lr=ARGS.learning_rate)
+    criterion = torch.nn.MSELoss()
+    log_name = f'{type(image_transformer).__name__}_{datetime.now().strftime("%m%d-%H")}'
 
-dataset = ImageFolder(data_root, transformer)
-# dataset = torch.utils.data.Subset(dataset, range(30000))
-data_loader = DataLoader(dataset=dataset, batch_size=batch_size, num_workers=8, drop_last=True)
+    trainer = Trainer(image_transformer=image_transformer,
+                      vgg=VGG,
+                      optimizer=optimizer,
+                      criterion=criterion,
+                      style=y_s,
+                      device=device,
+                      tensorboard_logger=log_name)
 
-# models
-VGG = MyVgg16()
-image_transformer = UNetSmall(batch_size)
-
-optimizer = optim.Adam(image_transformer.parameters(), lr=learning_rate)
-criterion = torch.nn.MSELoss()
-log_name = f'{type(image_transformer).__name__}_{datetime.now().strftime("%m%d-%H")}'
-
-trainer = TrainerMul(image_transformer=image_transformer,
-                vgg=VGG,
-                optimizer=optimizer,
-                criterion=criterion,
-                style=y_s,
-                device=device,
-                tensorboard_logger=log_name)
-
-trainer.train(data_loader, 2)
+    trainer.train(data_loader, 4, checkpoint=ARGS.checkpoint)
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Args Style Transfer Training')
+    parser.add_argument('--data_root', type=str, default='/content/data',
+                        help='Root directory of the dataset')
+    parser.add_argument('--style_folder', type=str, default='./mandostyles',
+                        help='Directory of the style images')
+    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3,
+                        help='Learning rate for ADAM')
+    parser.add_argument('-bs', '--batch_size', type=int, default=8,
+                        help='Batch size')
+    parser.add_argument('--image_size', type=int, default=256,
+                        help='Image size for the training')
+    parser.add_argument('--epochs', type=int, default=4,
+                        help='Number of epochs for training')
+    parser.add_argument('-cp', '--checkpoint', type=str, default=None,
+                        help='Checkpoint to load in case of continued training')
+    ARGS = parser.parse_args()
+    main(ARGS)
